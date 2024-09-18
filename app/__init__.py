b538b2d094ed3748cf56689ecbc37ef78a4dbe38
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2024-09-18 08:31:10 +0300

    modify and add files from chapter 12

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
